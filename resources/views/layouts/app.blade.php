2cd907be24524bb9d5d7663c9547748a9b9987b9
AnassLachiri <lachirianass@gmail.com>
2020-05-02 22:31:12 +0000

    Adding categores in the nav section

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,20 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                    <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    Categories <span class="caret"></span>
+                                </a>
 
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">
+                                        Clothes
+                                    </a>
+                                    <a class="dropdown-item" href="#">
+                                        Equipements
+                                    </a>
+                                </div>
+                            </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
