ba4ad6df0b85a5a0af3c1b6009eb6ffda22d8c22
LachiriAnass <lachirianass5@gmail.com>
2020-05-18 03:45:54 +0000

    Fixing the bug of an null rating value

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -7,7 +7,15 @@ use App\Comment;
 
 class CommentController extends Controller
 {
-    public function store(){
+    public function store(request $req){
+        $this->validate($req,[
+            'product_id'=>'required',
+            'user_id'=>'required',
+            'content'=>'required',
+            'rating'=>'required',
+        ]);
+
+
         $product_id = request('product_id');
         $user_id = request('user_id');
         $content = request('content');
@@ -17,6 +25,7 @@ class CommentController extends Controller
         $comment->content = $content;
         $comment->user_id = $user_id;
         $comment->product_id = $product_id;
+
         if($rating <= 5 && $rating >=0){
             $comment->rating = $rating;
         }else if($rating <0 ){
